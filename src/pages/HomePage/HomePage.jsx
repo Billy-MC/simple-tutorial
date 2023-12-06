01e15f5300ac081f3d12b742a508844ce62423b1
Billy <billychuim@gmail.com>
2023-12-06 11:17:46 +1100

    feat: s3

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,52 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 const HomePage = () => {
-	return <div>Home Page</div>;
+	const [file, setFile] = useState(null);
+	const [imageUrl, setImageUrl] = useState('');
+	console.log('🚀 ~ file: HomePage.jsx:6 ~ HomePage ~ imageUrl:', imageUrl);
+
+	const fileOnChangeHandler = (event) => {
+		setFile(event.target.files[0]);
+	};
+
+	const uploadFile = async () => {
+		if (!file) {
+			return;
+		}
+
+		try {
+			const uploadResponse = await axios.get('http://localhost:8000/api/v1/getPresignedUrl', {
+				params: { fileName: file.name, fileType: file.type },
+			});
+
+			const { presignedUrl: uploadUrl } = uploadResponse.data;
+
+			await axios.put(uploadUrl, file, {
+				headers: {
+					'Content-Type': file.type,
+				},
+			});
+
+			const downloadResponse = await axios.get(
+				'http://localhost:8000/api/v1/getDownloadPresignedUrl',
+				{
+					params: { fileName: file.name },
+				}
+			);
+
+			const { presignedUrl: downloadUrl } = downloadResponse.data;
+			setImageUrl(downloadUrl);
+		} catch (error) {
+			console.error(error.message);
+		}
+	};
+
+	return (
+		<div>
+			<input type='file' onChange={fileOnChangeHandler} />
+			<button onClick={uploadFile}>Upload</button>
+			{imageUrl ? <img src={imageUrl} alt='imageName' /> : null}
+		</div>
+	);
 };
 export default HomePage;
